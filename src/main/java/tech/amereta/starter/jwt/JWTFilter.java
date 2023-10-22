b42a63eb90ddf6312d659be4e06f4c7f4fe10b74
Soroush <shuoros@yahoo.com>
2023-10-22 22:50:58 +0200

    IMPLEMENT[JWTFilter]

---
+++ src/main/java/tech/amereta/starter/jwt/JWTFilter.java
@@ -0,0 +1,48 @@
+package tech.amereta.starter.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+import java.io.IOException;
+
+@Component
+public class JWTFilter extends OncePerRequestFilter {
+
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String COOKIE_NAME = "Authorization";
+
+    @Autowired
+    private JWTProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String jwt = resolveToken(request);
+        if (StringUtils.hasText(jwt) && jwtProvider.validateToken(jwt)) {
+            final Authentication authentication = jwtProvider.getAuthenticationToken(jwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        final String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        final Cookie cookieToken = WebUtils.getCookie(request, COOKIE_NAME);
+        if (cookieToken != null && StringUtils.hasText(cookieToken.getValue())) {
+            return cookieToken.getValue();
+        }
+        return null;
+    }
+}
